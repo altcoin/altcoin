e7d09f4cea25293d1ae7391036bd0a3b35f6723b
AltCoin <altcoinme@gmail.com>
2013-05-16 14:10:20 +1000

    More name updataes. I probably should have just automated this.

--- src/main.cpp
+++ src/main.cpp
@@ -1903,7 +1903,7 @@ bool CheckDiskSpace(uint64 nAdditionalBytes)
         string strMessage = _("Warning: Disk space is low");
         strMiscWarning = strMessage;
         printf("*** %s\n", strMessage.c_str());
-        uiInterface.ThreadSafeMessageBox(strMessage, "Litecoin", CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
+        uiInterface.ThreadSafeMessageBox(strMessage, "Altcoin", CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
         StartShutdown();
         return false;
     }
