937d08ccceb3d25a702be88444f0e3b0baddadab
AltCoin <altcoinme@gmail.com>
2013-05-16 14:05:20 +1000

    Update packet header.

--- src/main.cpp
+++ src/main.cpp
@@ -2353,12 +2353,7 @@ bool static AlreadyHave(CTxDB& txdb, const CInv& inv)
 }
 
 
-
-
-// The message start string is designed to be unlikely to occur in normal data.
-// The characters are rarely used upper ascii, not valid as UTF-8, and produce
-// a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Litecoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 'a', 'l', 't', 'c' }; // Altcoin message start string
 
 
 bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
